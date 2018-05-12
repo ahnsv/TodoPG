3895ef83744e6fa244d822f4d6474680b3d970e0
Humphrey Ahn <ahnsv@bc.edu>
2018-05-12 15:30:18 +0900

    Vue-cli webpack template + Vuex + Mocha Init

---
+++ vueapp01/src/App.vue
@@ -0,0 +1,47 @@
+<template>
+  <div id="app">
+    <!-- <img src="./assets/logo.png"> -->
+    <Todoapp :todos="todos"></Todoapp>
+  </div>
+</template>
+<script>
+import Todoapp from "./components/TodoApp";
+export default {
+  name: "App",
+  components: {
+    Todoapp
+  },
+  data() {
+    return {
+      todos: [{
+        title: 'Todo A',
+        project: 'Project A',
+        done: false,
+      }, {
+        title: 'Todo B',
+        project: 'Project B',
+        done: true,
+      }, {
+        title: 'Todo C',
+        project: 'Project C',
+        done: false,
+      }, {
+        title: 'Todo D',
+        project: 'Project D',
+        done: false,
+      }],
+    }
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ vueapp01/src/components/TodoApp.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="container">
+        <ul class="list-group">
+            <li class="list-group-item" :todos="todos" v-for="todo in todos"> {{todo.title}} </li>
+        </ul> 
+    </div>
+</template>
+<script>
+export default {
+  props: ['todos']
+}
+</script>
+<style scoped>
+
+</style>
+
